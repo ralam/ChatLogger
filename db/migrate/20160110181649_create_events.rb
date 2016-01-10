8531fa58bc50d21c53f5d55d6bd66880ea0139a8
ralam <rehan.alam@gmail.com>
2016-01-10 10:20:47 -0800

    create event model

---
+++ db/migrate/20160110181649_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.datetime :date, null: false
+      t.string :user, null: false
+      t.string :type, null: false
+      t.string :message
+      t.string :otheruser
+      t.timestamps null: false
+    end
+  end
+end
