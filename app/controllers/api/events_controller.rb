fb2fe029c40734405d9aa1ce0049ccc7b2db81ac
ralam <rehan.alam@gmail.com>
2016-01-10 10:51:42 -0800

    add create method to events controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,10 +1,22 @@
 class Api::EventsController < ApplicationController
+
+  def create
+    @event = Event.new(event_params)
+    if @event.create
+      render json: {"status": "ok"}, status: 200
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
+  end
+
   def index
   end
 
   private
 
   def event_params
-    params.require(:from, :to)
+    params
+      .require(:event)
+      .permit(:date, :user, :type, :message, :otheruser)
   end
 end
