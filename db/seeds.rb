7222122480fe27ddb8211241a4eb06403da15c2b
ralam <rehan.alam@gmail.com>
2016-01-10 15:27:36 -0800

    add seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.create(
+  {"date": "2014-02-28T13:00Z", "user": "Alice", "type": "enter"},
+  {"date":"2014-02-28T13:01Z", "user": "Alice", "type": "comment", "message": "hello, this is a sample message"},
+  {"date": "2014-02-28T13:02Z", "user": "Alice", "type": "highfive", "otheruser": "Bob"},
+  {"date": "2014-02-28T13:03Z", "user": "Alice", "type": "leave"})
