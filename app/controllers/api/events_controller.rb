39ad30de47b92500b19a63828c2278d4b1ca1691
ralam <rehan.alam@gmail.com>
2016-01-10 11:01:41 -0800

    add index method to api/events controller"

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -10,6 +10,13 @@ class Api::EventsController < ApplicationController
   end
 
   def index
+    query_params = query_params
+    @events = Event.where(:date > query_params[:from] AND :date < query_params[:to])
+    if @events
+      render json: @events, status: 200
+    else
+      render json: "No events found", status: 404
+    end
   end
 
   private
@@ -18,5 +25,10 @@ class Api::EventsController < ApplicationController
     params
       .require(:event)
       .permit(:date, :user, :type, :message, :otheruser)
+  end
+
+  def query_params
+    params
+      .require(:event).permit(:from, :to)
   end
 end
