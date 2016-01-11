54ddd4ec08d62fe2894cf51fab5e640ac492b65e
ralam <rehan.alam@gmail.com>
2016-01-10 21:57:48 -0800

    set up factory for events

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
-  factory :event do
-    
+  factory :event do |f|
+    f.date Faker::Time.backward(5)
+    f.user Faker::Internet.user_name
+    f.type_of "comment"
+    f.message Faker::Lorem.words
+    f.otheruser Faker::Internet.user_name
   end
 
 end
